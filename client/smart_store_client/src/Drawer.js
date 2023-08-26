10e17452191427bc9794a7859dfc860bdd0c96da
Jashwanth Kadaru <jashwanthkadaru2021@gmail.com>
2023-08-26 09:11:44 +0530

    Inside client folder, created react side drawer and css styles for the drawer.

--- client/smart_store_client/src/Drawer.js
+++ client/smart_store_client/src/Drawer.js
@@ -3,7 +3,42 @@ import './Drawer.css'
 const Drawer = () => {
     return (
         <div className="app-drawer">
-           
+            <div className='navbar'>
+                <div className='brand-div'>
+                    <div className='brand-name'>
+                        <h1> Smart </h1>
+                        <span> Fashion </span> 
+                        <h1> Store </h1>
+                    </div>
+
+                    <div className='brand-tag'>
+                        <h3> Fashion </h3>
+                        <h3> made </h3>
+                        <span> smarter </span>
+                    </div>
+                </div>
+
+                <div className='user-div'>
+                    <div className='profile-pic'> <img src=''/> </div>
+                    <div className='user-details'>
+                        <h4> Kadaru Jashwanth Reddy</h4>
+                        <h6> #0001102FA137 </h6>
+                    </div>
+                </div>
+
+                <header>
+                    <nav>
+                        <NavLink className='link' to={'/shop'}> Home </NavLink>
+                        <NavLink className='link' to={'/shop'}> Home </NavLink>
+                        <NavLink className='link' to={'/shop'}> Home </NavLink>
+                        <NavLink className='link' to={'/'}> Signout </NavLink>
+                    </nav>
+                </header>
+            </div>
+
+            <footer>
+                <p> Made with &hearts; by Jashwanth Kadaru</p>
+            </footer>
         </div>
     )
 }
