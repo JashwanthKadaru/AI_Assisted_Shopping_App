85b1208249e703d2129f3801cd6b38d232e7d177
Jashwanth Kadaru <jashwanthkadaru2021@gmail.com>
2023-08-26 16:30:08 +0530

    Inside client folder, made changes too directory structure.

--- client/smart_store_client/src/App.js
+++ client/smart_store_client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Drawer from './Drawer';
+import Drawer from './components/Drawer';
 import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
 function App() {
--- client/smart_store_client/src/components/Drawer.js
+++ client/smart_store_client/src/components/Drawer.js
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import './Drawer.css'
+import '../css/Drawer.css'
 const Drawer = () => {
     return (
         <div className="app-drawer">
